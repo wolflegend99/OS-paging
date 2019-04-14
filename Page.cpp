841002d01e21f30f9cd440fd66fb8d7d6b3de2c1
Vishal Saranathan <33394085+VishalS99@users.noreply.github.com>
2019-04-14 18:50:32 +0530

    Create Page.cpp

---
+++ Page.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#include <chrono>
+using namespace std;
+
+#define INITIAL_CHOICE 1;
+#define INITIAL 1;
+
+int main(int argc, char* argv[]) {
+
+    auto start = std::chrono::high_resolution_clock::now();
+    unsigned long long address;
+    unsigned long page_number;
+    unsigned long offset;
+    bool choice = INITIAL_CHOICE; 
+    int status = INITIAL; 
+
+    while(1) {
+
+        switch (choice) {
+
+            case 1 : {
+
+                if(status){ 
+
+                    address = atoll(argv[1]); 
+                    
+                    // PAGE SIZE GIVEN IS 4KB
+                    
+                    page_number = address >> 12; 
+                    
+                    offset = address & 0xfff; 
+
+                    cout << "ADDRESS:\t PAGE NUMBER: \t OFFSET: \n";
+                    cout << "----------------------------------------" << endl;
+                    cout << address << "\t\t\t" << page_number << "\t " << offset << endl;
+
+                } else {
+
+                    string address_string;
+                    cout << "Enter the address: " ;
+                    cin >> address_string;
+
+                    address = stoll(address_string);
+
+                    // PAGE SIZE GIVEN IS 4KB
+                    
+                    page_number = address >> 12;
+                    
+                    offset = address & 0xfff;
+
+                    cout << "ADDRESS:\t PAGE NUMBER: \t OFFSET: \n";
+                    cout << "----------------------------------------" << endl;
+                    cout << address << "\t\t\t" << page_number << "\t " << offset << endl;
+
+                }
+                break;
+            }
+
+            case 0 : {
+
+                cout << "#### PROGRAM TERMINATION ####" << endl;
+
+                auto end = std::chrono::high_resolution_clock::now();
+                std::chrono::duration<double> elapsed = end - start; 
+
+                cout << "EXECUTION TIME: " << elapsed.count() << " sec" << endl;
+                exit(0);
+            }
+
+        }
+        cout << "Input choice to proceed (0/1) : ";
+        cin >> choice;
+
+        status = 0;
+    }
+    
+    return 0;
+}
